1c8e14693c561a0070ff87f61f982a981df133a0
codayoda36 <116090022+codayoda36@users.noreply.github.com>
2023-11-10 23:56:01 +0100

    Update sensor.py

--- custom_components/awesome/sensor.py
+++ custom_components/awesome/sensor.py
@@ -33,15 +33,16 @@ def setup_platform(
         from_station = route.get("from_station", "")
         to_station = route.get("to_station", "")
         sensor_name = route.get("sensor_name", "")
+        travel_time = route.get("travel_time", "")
 
-        sensors.append(ExampleSensor(hass, api_key, from_station, to_station, min_departure_threshold, sensor_name, update_frequency))
+        sensors.append(ExampleSensor(hass, api_key, from_station, to_station, min_departure_threshold, sensor_name, update_frequency, travel_time))
 
     add_entities(sensors)
 
 class ExampleSensor(SensorEntity):
     """Representation of a Sensor."""
 
-    def __init__(self, hass: HomeAssistant, api_key: str, from_station: str, to_station: str, min_departure_threshold: int, sensor_name: str, update_frequency: int):
+    def __init__(self, hass: HomeAssistant, api_key: str, from_station: str, to_station: str, min_departure_threshold: int, sensor_name: str, update_frequency: int, travel_time: str):
         self._hass = hass
         self._api_key = api_key
         self._from_station = from_station
@@ -49,6 +50,7 @@ class ExampleSensor(SensorEntity):
         self._min_departure_threshold = min_departure_threshold
         self._sensor_name = sensor_name
         self._update_frequency = update_frequency
+        self._travel_time = self.parse_time(travel_time)
 
         self._arrival_time_planned_attribute = None
         self._arrival_time_actual_attribute = None
@@ -64,6 +66,12 @@ class ExampleSensor(SensorEntity):
         self._arrival_platform_planned_attribute = 0
         self._arrival_platform_actual_attribute = 0
         self._last_updated = datetime.now(pytz.timezone("Europe/Amsterdam")).strftime("%H:%M")
+
+    def parse_time(self, time_str):
+        """Parse the time string and return a datetime object."""
+        if time_str:
+            return datetime.strptime(time_str, "%H:%M").time()
+        return None
 
     @property
     def name(self):
@@ -102,60 +110,71 @@ class ExampleSensor(SensorEntity):
 
     async def async_update(self):
         """Fetch new state data for the sensor."""
-        base_url = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips"
+        current_time = datetime.now(pytz.timezone("Europe/Amsterdam")).time()
 
-        params = {
-            "fromStation": self._from_station,
-            "toStation": self._to_station
-            # Add more parameters as needed
-        }
-        headers = {
-            "Ocp-Apim-Subscription-Key": self._api_key
-            # Replace YOUR_SUBSCRIPTION_KEY with your actual subscription key
-        }
+        # Check if the current time is within the specified travel time window
+        if self.is_within_travel_window(current_time):
+            base_url = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips"
 
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(base_url, params=params, headers=headers) as response:
-                    if response.status == 200:
-                        trip_data = await response.json()
+            params = {
+                "fromStation": self._from_station,
+                "toStation": self._to_station
+                # Add more parameters as needed
+            }
+            headers = {
+                "Ocp-Apim-Subscription-Key": self._api_key
+                # Replace YOUR_SUBSCRIPTION_KEY with your actual subscription key
+            }
 
-                        current_time = datetime.now(pytz.timezone("Europe/Amsterdam"))
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(base_url, params=params, headers=headers) as response:
+                        if response.status == 200:
+                            trip_data = await response.json()
 
-                        for trip in trip_data['trips']:
-                            leg = trip['legs'][0]  # Assuming there is only one leg in the trip
-                            departure_time_planned = datetime.strptime(leg['origin']['plannedDateTime'], "%Y-%m-%dT%H:%M:%S%z")
-                            departure_time_actual = datetime.strptime(leg['origin']['actualDateTime'], "%Y-%m-%dT%H:%M:%S%z") if 'actualDateTime' in leg['origin'] else departure_time_planned
-                            
-                            arrival_time_planned = datetime.strptime(leg['destination']['plannedDateTime'], "%Y-%m-%dT%H:%M:%S%z")
-                            arrival_time_actual = datetime.strptime(leg['destination']['actualDateTime'], "%Y-%m-%dT%H:%M:%S%z") if 'actualDateTime' in leg['destination'] else self._arrival_time_planned_attribute
+                            for trip in trip_data['trips']:
+                                leg = trip['legs'][0]  # Assuming there is only one leg in the trip
+                                departure_time_planned = datetime.strptime(leg['origin']['plannedDateTime'], "%Y-%m-%dT%H:%M:%S%z")
+                                departure_time_actual = datetime.strptime(leg['origin']['actualDateTime'], "%Y-%m-%dT%H:%M:%S%z") if 'actualDateTime' in leg['origin'] else departure_time_planned
+                                
+                                arrival_time_planned = datetime.strptime(leg['destination']['plannedDateTime'], "%Y-%m-%dT%H:%M:%S%z")
+                                arrival_time_actual = datetime.strptime(leg['destination']['actualDateTime'], "%Y-%m-%dT%H:%M:%S%z") if 'actualDateTime' in leg['destination'] else self._arrival_time_planned_attribute
 
-                            if departure_time_planned > current_time + timedelta(minutes=self._min_departure_threshold):
-                                next_trip = trip
-                                break
-                        
-                        if 'next_trip' in locals():
-                            self._arrival_time_planned_attribute = arrival_time_planned.strftime("%H:%M")
-                            self._arrival_time_actual_attribute = arrival_time_actual.strftime("%H:%M")
+                                if departure_time_planned > datetime.now(pytz.timezone("Europe/Amsterdam")) + timedelta(minutes=self._min_departure_threshold):
+                                    next_trip = trip
+                                    break
+                                
+                            if 'next_trip' in locals():
+                                self._arrival_time_planned_attribute = arrival_time_planned.strftime("%H:%M")
+                                self._arrival_time_actual_attribute = arrival_time_actual.strftime("%H:%M")
 
-                            self._departure_time_planned_attribute = departure_time_planned.strftime("%H:%M")
-                            self._departure_time_actual_attribute = departure_time_actual.strftime("%H:%M")
+                                self._departure_time_planned_attribute = departure_time_planned.strftime("%H:%M")
+                                self._departure_time_actual_attribute = departure_time_actual.strftime("%H:%M")
 
-                            self._departure_delay_attribute = (departure_time_actual - departure_time_planned).total_seconds() / 60 if departure_time_actual else None
-                            self._arrival_delay_attribute = (arrival_time_actual - arrival_time_planned).total_seconds() / 60 if arrival_time_actual else None
-                            self._travel_time_attribute = (arrival_time_actual - departure_time_actual).total_seconds() / 60 if arrival_time_actual and departure_time_actual else None
+                                self._departure_delay_attribute = (departure_time_actual - departure_time_planned).total_seconds() / 60 if departure_time_actual else None
+                                self._arrival_delay_attribute = (arrival_time_actual - arrival_time_planned).total_seconds() / 60 if arrival_time_actual else None
+                                self._travel_time_attribute = (arrival_time_actual - departure_time_actual).total_seconds() / 60 if arrival_time_actual and departure_time_actual else None
 
-                            self._transfers_attribute = trip['transfers']
-                            self._departure_platform_planned_attribute = leg['origin']['plannedTrack']
-                            self._departure_platform_actual_attribute = leg['origin']['actualTrack'] if 'actualTrack' in leg['origin'] else 'Not available'
-                            self._arrival_platform_planned_attribute = leg['destination']['plannedTrack']
-                            self._arrival_platform_actual_attribute = leg['destination']['actualTrack'] if 'actualTrack' in leg['destination'] else 'Not available'
-                            self._last_updated = datetime.now(pytz.timezone("Europe/Amsterdam")).strftime("%H:%M")
+                                self._transfers_attribute = trip['transfers']
+                                self._departure_platform_planned_attribute = leg['origin']['plannedTrack']
+                                self._departure_platform_actual_attribute = leg['origin']['actualTrack'] if 'actualTrack' in leg['origin'] else 'Not available'
+                                self._arrival_platform_planned_attribute = leg['destination']['plannedTrack']
+                                self._arrival_platform_actual_attribute = leg['destination']['actualTrack'] if 'actualTrack' in leg['destination'] else 'Not available'
+                                self._last_updated = datetime.now(pytz.timezone("Europe/Amsterdam")).strftime("%H:%M")
 
-                    else:
-                        _LOGGER.error("Error with the api call: %s", await response.text())
+                        else:
+                            _LOGGER.debug("Error with the api call: %s", await response.text())
 
-        except Exception as e:
-            _LOGGER.error("Error making request to NS API: %s", e)
-        
-        self.async_write_ha_state()
+            except Exception as e:
+                _LOGGER.error("Error making request to NS API: %s", e)
+            
+            self.async_write_ha_state()
+
+    def is_within_travel_window(self, current_time):
+        """Check if the current time is within the specified travel time window."""
+        if self._travel_time:
+            travel_time_start = datetime.combine(datetime.now().date(), self._travel_time) - timedelta(minutes=30)
+            travel_time_end = datetime.combine(datetime.now().date(), self._travel_time) + timedelta(minutes=30)
+            return travel_time_start.time() <= current_time <= travel_time_end.time()
+        else:
+            return True
