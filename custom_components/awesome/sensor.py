97eab8d768b0eb0c49cf2b27cbda26eeb92bfce0
codayoda36 <116090022+codayoda36@users.noreply.github.com>
2023-11-10 21:28:30 +0100

    Update sensor.py

--- custom_components/awesome/sensor.py
+++ custom_components/awesome/sensor.py
@@ -1,18 +1,6 @@
-import asyncio
-import random
-import requests
-import logging
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-)
-from homeassistant.const import UnitOfTemperature
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
-_LOGGER = logging.getLogger(__name__)
+from datetime import datetime
+import pytz
+from homeassistant.helpers.template import Template
 
 async def async_track_time_interval(hass, interval, action):
     while True:
@@ -26,15 +14,16 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
-
+    add_entities([ExampleSensor(hass)])
 
 class ExampleSensor(SensorEntity):
     """Representation of a Sensor."""
 
     _attr_name = "Examplesensor"
 
-    _test_attribute = random.choice("abcdefghijklmnopqrstuvwxyz")
+    def __init__(self, hass: HomeAssistant):
+        self._hass = hass
+        self._test_attribute = random.choice("abcdefghijklmnopqrstuvwxyz")
 
     @property
     def name(self):
@@ -60,10 +49,7 @@ class ExampleSensor(SensorEntity):
         self._state_update_task.cancel()
 
     async def async_update(self):
-        """Fetch new state data for the sensor.
-        _LOGGER.debug("Updating atributes")
-        This is the only method that should fetch new data for Home Assistant.
-        """
+        """Fetch new state data for the sensor."""
         base_url = "https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/trips"
 
         params = {
@@ -71,15 +57,13 @@ class ExampleSensor(SensorEntity):
             "toStation": "shl"
             # Add more parameters as needed
         }
-        _LOGGER.debug("Setting params")
         headers = {
             "Ocp-Apim-Subscription-Key": "8437a73330144f1b82320e22b351af61"
             # Replace YOUR_SUBSCRIPTION_KEY with your actual subscription key
         }
-        _LOGGER.debug("Setting headers")
 
         try:
-            response = requests.get(base_url, params=params, headers=headers)
+            response = await self.hass.async_add_executor_job(requests.get, base_url, params=params, headers=headers)
 
             if response.status_code == 200:
                 trip_data = response.json()
@@ -92,7 +76,6 @@ class ExampleSensor(SensorEntity):
                     departure_time_planned = datetime.strptime(leg['origin']['plannedDateTime'], "%Y-%m-%dT%H:%M:%S%z")
                     departure_time_actual = datetime.strptime(leg['origin']['actualDateTime'], "%Y-%m-%dT%H:%M:%S%z") if 'actualDateTime' in leg['origin'] else None
 
-                    _LOGGER.debug("Setting attribute value")
                     self._test_attribute = random.choice("abcdefghijkl")
             else:
                 _LOGGER.debug("Error with the api call: %s", response.text)
@@ -100,4 +83,5 @@ class ExampleSensor(SensorEntity):
         except Exception as e:
             _LOGGER.error("Error making request to NS API: %s", e)
             self._test_attribute = "error"
+        
         self.async_write_ha_state()
